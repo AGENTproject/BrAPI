f2a454dbc3aafd266afd4729c74bca85aef46cc0
Gouripriya-Davuluri <davulurigouripriya@gmail.com>
2024-02-15 13:57:10 +0100

    Add Pagination
    Added Pagination and Implemented “get /samples/{sampleDbId}”

--- brapi_bp.py
+++ brapi_bp.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
 import oracledb
+from flask import request
 import os
 
 brapi_bp = Blueprint('brapi_bp', __name__,url_prefix='/genotyping/brapi/v2')
@@ -76,10 +77,11 @@ def get_samples():
     res_context = None
     res_datafiles = []
     res_status = []
-    res_page_size = 10
-    res_total_count = 0
-    res_total_pages = 0
-    res_current_page = 0
+    
+     # Get page size and page number from query parameters
+    res_page_size = int(request.args.get('pageSize', 10))
+    res_current_page = int(request.args.get('currentPage', 1))
+
     samples = []
     total_count = 0
     with oracledb.connect(user=DB_USER, password=DB_PASSWORD, host=DB_HOST, service_name=DB_SERVICE_NAME) as connection:
@@ -89,6 +91,13 @@ def get_samples():
                 samples.append(
                     {'additionalInfo': r[0], 'column': r[1], 'externalReferences': [{"referenceId":r[2],"referenceSource":  ""}], 'germplasmDbId': r[3], 'observationUnitDbId': r[4], 'plateDbId': r[5], 'plateName': r[6], 'programDbId': r[7], 'row': r[8], 'sampleBarcode': r[9], 'sampleDbId':  str(r[10]), 'sampleDescription': r[11], 'sampleGroupDbId': r[12], 'sampleName': r[13], 'samplePUI': r[14], 'sampleTimestamp': r[15], 'sampleType': r[16], 'studyDbId': r[17], 'takenBy': r[18], 'tissueType': r[19], 'trialDbId': r[20], 'well': r[21]})
     res_total_count = len(samples)
+    res_total_pages = (total_count + res_page_size - 1) // res_page_size
+
+    # Apply pagination to samples
+    start_index = (res_current_page - 1) * res_page_size
+    end_index = start_index + res_page_size
+    paginated_samples = samples[start_index:end_index]
+    
     
     return {
          "@context": res_context,
@@ -104,7 +113,7 @@ def get_samples():
           },
         "totalcount": total_count,
         "result": {
-            "data": samples
+            "data": paginated_samples
         }
     }
 
@@ -116,7 +125,7 @@ def get_sample_by_reference(reference_id):
         with connection.cursor() as cursor:
             sql = """SELECT "additionalInfo", "column", "externalReferences", "germplasmDbId", "observationUnitDbId", "plateDbId", "plateName", "programDbId", "row", "sampleBarcode", "sampleDbId", "sampleDescription", "sampleGroupDbId", "sampleName", "samplePUI", "sampleTimestamp", "sampleType", "studyDbId", "takenBy", "tissueType", "trialDbId", "well" FROM mv_brapi_samples"""
             for r in cursor.execute(sql):
-                if r[2] == reference_id:
-                    sample = {'additionalInfo': r[0], 'column': r[1], 'externalReferences':[{"referenceId":r[2],"referenceSource":  ""}], 'germplasmDbId': r[3], 'observationUnitDbId': r[4], 'plateDbId': r[5], 'plateName': r[6], 'programDbId': r[7], 'row': r[8], 'sampleBarcode': r[9], 'sampleDbId': str(r[10]),
+                if r[2] == reference_id or str(r[10]) == reference_id:
+                    sample = {'additionalInfo': r[0], 'column': r[1], 'externalReferences':[{"referenceId":r[2],"referenceSource":  ""}], 'germplasmDbId': str("r[3]"), 'observationUnitDbId': r[4], 'plateDbId': r[5], 'plateName': r[6], 'programDbId': r[7], 'row': r[8], 'sampleBarcode': r[9], 'sampleDbId': str(r[10]),
                               'sampleDescription': r[11], 'sampleGroupDbId': r[12], 'sampleName': r[13], 'samplePUI': r[14], 'sampleTimestamp': r[15], 'sampleType': r[16], 'studyDbId': r[17], 'takenBy': r[18], 'tissueType': r[19], 'trialDbId': r[20], 'well': r[21]}
     return sample
