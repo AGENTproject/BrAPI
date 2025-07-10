55dbaee9518e93be6bf21f4d7e7265db253bc89a
unknown <joseph.ruff@btinternet.com>
2025-07-10 14:46:13 +0100

    fixed search funtionality on attributes endpoint

--- brapi_bp.py
+++ brapi_bp.py
@@ -1048,7 +1048,7 @@ def get_attributes():
             if is_number(value):
                  where_clause += f'"{key.upper()}" = \'{value}\'' 
             else:
-                where_clause +=  f'"{key}" = \'{value}\''
+                where_clause +=  f'"{key.upper()}" = \'{value}\''
 
     attributes = []
 
